81ce8f31fce7873f42eafcd918fb0ccb622346dc
최근원 <kidjustinchoi@gmail.com>
2023-07-21 00:27:54 +0900

    del lotties

--- src/lotties/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as CheckLottie } from './checkLottie.json';
-export { default as LoadingLottie } from './loadingLottie.json';
-export { default as NonePageLottie } from './404Lottie.json';
-export { default as NoDataLottie } from './noDataLottie.json';
-export { default as FailLottie } from './failLottie.json';
