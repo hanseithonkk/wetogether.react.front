1862a439ea329606165d553ba5cb7f1a4d8e3347
최근원 <kidjustinchoi@gmail.com>
2023-07-21 11:08:16 +0900

    feat: add map

--- src/pages/group-location/index.tsx
+++ src/pages/group-location/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 import { CreateMeetingTitle } from '@/components';
 import { UseGetGroupLocation, useGetGroup } from '@/api/query/useGroup';
@@ -18,16 +19,33 @@ export const GroupLocationPage: React.FC = () => {
     <>
       {data ? (
         <S.GroupLocationPageContainer>
-          <S.GroupLocationPageTopSection>
-            <CreateMeetingTitle
-              text={
-                <>
-                  {data.length}명이 <br />
-                  모임으로 가고 있어요
-                </>
-              }
+          <CreateMeetingTitle
+            text={
+              <>
+                {data.length}명이 <br />
+                모임으로 가고 있어요
+              </>
+            }
+          />
+          <MapContainer
+            center={[37.55179, 126.95171]}
+            zoom={20}
+            scrollWheelZoom={true}
+            style={{ width: '100%', height: '100%', zIndex: '10' }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-          </S.GroupLocationPageTopSection>
+            {data?.map((value) => {
+              const position = value.location.split(',');
+              return (
+                <Marker position={[+position[1], +position[0]]}>
+                  <Popup>{value.title}</Popup>
+                </Marker>
+              );
+            })}
+          </MapContainer>
         </S.GroupLocationPageContainer>
       ) : (
         <>
--- src/pages/group-location/styled.ts
+++ src/pages/group-location/styled.ts
@@ -5,14 +5,6 @@ export const GroupLocationPageContainer = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  align-items: flex-start;
   padding: 2rem;
-`;
-
-export const GroupLocationPageTopSection = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
   row-gap: 1.2rem;
 `;
